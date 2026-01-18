142487e785cbbb476862145e6f2a5554970d0241
hxabcd <hxabcd1234@outlook.com>
2026-01-18 15:57:27 +0800

    update: 1.1.0

--- consts.py
+++ consts.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from packaging.version import Version
 
-VERSION = Version("1.1.0a2")
+VERSION = Version("1.1.0")
 SENTRY_DSN = "https://992aafe788df5155ed58c1498188ae6b@o4510727360348160.ingest.us.sentry.io/4510727362248704"
 MANIFEST_URL = "https://0xabcd.dev/update/EasiAuto.json"
 
