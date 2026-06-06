8e5fffd404144685e32eea525f5fbd2f4f2c11f7
hxabcd <hxabcd1234@outlook.com>
2026-06-06 21:06:11 +0800

    Revert "feat: 使用注册机制管理登录策略"
    This reverts commit 119c5eee9f2e1bc4ba80a40fa2f3bea9ead0429c.

--- src/EasiAuto/core/automator/cv.py
+++ src/EasiAuto/core/automator/cv.py
@@ -4,13 +4,11 @@ from loguru import logger
 
 from EasiAuto.consts import IS_FULL
 from EasiAuto.core.utils import Point, get_resource, get_scale
-from EasiAuto.models.config import LoginMethod, config
+from EasiAuto.models.config import config
 
 from .base import LoginError, PyAutoGuiBaseAutomator
-from .registry import register
 
 
-@register(LoginMethod.CV)
 class CVAutomator(PyAutoGuiBaseAutomator):
     """通过识别图像登录"""
 
--- src/EasiAuto/core/automator/manager.py
+++ src/EasiAuto/core/automator/manager.py
@@ -2,8 +2,7 @@ from loguru import logger
 
 from PySide6.QtCore import QObject, Signal
 
-from EasiAuto.core.automator import BaseAutomator
-from EasiAuto.core.automator.registry import get_automator_class
+from EasiAuto.core.automator import BaseAutomator, CVAutomator, FixedAutomator, InjectAutomator, UIAAutomator
 from EasiAuto.models.config import LoginMethod, config
 
 
@@ -21,12 +20,13 @@ class AutomationManager(QObject):
         self._automator: BaseAutomator | None = None
 
     def _get_strategy_class(self, strategy: LoginMethod) -> type[BaseAutomator]:
-        cls = get_automator_class(strategy)
-        if cls is None:
-            from EasiAuto.core.automator.fixed import FixedAutomator
-
-            cls = FixedAutomator  # DEFAULT
-        return cls
+        strategies: dict[LoginMethod, type[BaseAutomator]] = {
+            LoginMethod.FIXED: FixedAutomator,
+            LoginMethod.CV: CVAutomator,
+            LoginMethod.UIA: UIAAutomator,
+            LoginMethod.INJECT: InjectAutomator,
+        }
+        return strategies.get(strategy, FixedAutomator)
 
     def run(self, account: str, password: str):
         if self._automator and self._automator.isRunning():
