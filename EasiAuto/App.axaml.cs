95053806b30e7dfd41e6b49ba8b766ddac734f10
hxabcd <hxabcd1234@outlook.com>
2026-07-28 01:49:29 +0800

    chore: 注册 SeewoClient 到 DI，更新项目引用
    - App.axaml.cs: 注册 ConfigService / LoginConfig / SeewoClient / BaseLoginStrategy
    - csproj: 更新包引用和项目依赖

--- EasiAuto/App.axaml.cs
+++ EasiAuto/App.axaml.cs
@@ -1,16 +1,23 @@
+using System;
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
-using System.Linq;
 using Avalonia.Markup.Xaml;
+using EasiAuto.Core.Services;
+using EasiAuto.Core.Services.Seewo;
+using EasiAuto.Core.Services.Automation.Strategies;
 using EasiAuto.ViewModels;
 using EasiAuto.Views;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EasiAuto;
 
 public partial class App : Application
 {
+    public static IServiceProvider Services { get; private set; } = null!;
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -18,6 +25,22 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        var services = new ServiceCollection();
+
+        // 注册服务
+        services.AddSingleton(sp =>
+        {
+            var configService = new ConfigService();
+            configService.LoadConfig();
+            return configService;
+        });
+        services.AddTransient(sp =>
+            sp.GetRequiredService<ConfigService>().Config.Login);
+        services.AddSingleton<SeewoClient>();
+        services.AddTransient<BaseLoginStrategy>();
+
+        Services = services.BuildServiceProvider();
+
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow
